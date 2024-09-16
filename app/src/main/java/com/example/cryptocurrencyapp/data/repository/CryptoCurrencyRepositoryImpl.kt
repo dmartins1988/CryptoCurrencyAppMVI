9f13654a94609cd00570755ddd791436c9743423
dmartins1988 <d.martins1988@gmail.com>
2024-09-16 23:45:33 +0100

    Add implementation of repository on data layer

---
+++ app/src/main/java/com/example/cryptocurrencyapp/data/repository/CryptoCurrencyRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.example.cryptocurrencyapp.data.repository
+
+import com.example.cryptocurrencyapp.core.DataError
+import com.example.cryptocurrencyapp.core.Result
+import com.example.cryptocurrencyapp.data.models.toCoin
+import com.example.cryptocurrencyapp.data.remote.CryptoCurrencyService
+import com.example.cryptocurrencyapp.di.IODispatcher
+import com.example.cryptocurrencyapp.domain.CryptoCurrencyRepository
+import com.example.cryptocurrencyapp.domain.models.Coin
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class CryptoCurrencyRepositoryImpl @Inject constructor(
+    private val service: CryptoCurrencyService,
+    @IODispatcher private val ioScope: CoroutineDispatcher
+) : CryptoCurrencyRepository {
+    override suspend fun getCoins(): Result<List<Coin>, DataError.Network> {
+        return withContext(ioScope) {
+            try {
+                val listOfCoins = service.getCoins().map { it.toCoin() }
+                Result.Success(data = listOfCoins)
+            } catch (e: HttpException) {
+                when (e.code()) {
+                    408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
+                    413 -> Result.Error(DataError.Network.PAYLOAD_TOO_LARGE)
+                    else -> Result.Error(DataError.Network.UNKNOWN)
+                }
+            } catch (e: IOException) {
+                Result.Error(DataError.Network.NO_INTERNET)
+            }
+        }
+    }
+}
