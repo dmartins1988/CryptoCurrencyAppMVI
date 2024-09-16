0c2eb0e3e42b92d958fc239eccdd9e8ed4e10f3b
dmartins1988 <d.martins1988@gmail.com>
2024-09-17 00:15:58 +0100

    Add new request service for coin detail

--- app/src/main/java/com/example/cryptocurrencyapp/data/remote/CryptoCurrencyService.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/data/remote/CryptoCurrencyService.kt
@@ -1,7 +1,9 @@
 package com.example.cryptocurrencyapp.data.remote
 
+import com.example.cryptocurrencyapp.data.models.CoinDetailDto
 import com.example.cryptocurrencyapp.data.models.CoinDto
 
 interface CryptoCurrencyService {
-    suspend fun getCoins() : List<CoinDto>
+    suspend fun getCoins(): List<CoinDto>
+    suspend fun getCoinDetail(coinId: String): CoinDetailDto
 }
--- app/src/main/java/com/example/cryptocurrencyapp/data/remote/CryptoCurrencyServiceImpl.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/data/remote/CryptoCurrencyServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.cryptocurrencyapp.data.remote
 
+import com.example.cryptocurrencyapp.data.models.CoinDetailDto
 import com.example.cryptocurrencyapp.data.models.CoinDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -11,5 +12,9 @@ class CryptoCurrencyServiceImpl @Inject constructor(
 ) : CryptoCurrencyService {
     override suspend fun getCoins(): List<CoinDto> {
         return httpClient.get("/v1/coins").body()
+    }
+
+    override suspend fun getCoinDetail(coinId: String): CoinDetailDto {
+        return httpClient.get("/v1/coins/$coinId").body()
     }
 }
